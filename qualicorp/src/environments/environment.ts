68ba758a536752d1af2717465cd3cd43e6782502
Matheus Pacheco <matheus_pachecosilva@hotmail.com>
2021-09-08 04:48:10 -0300

    Ajustado layout dos cards na tela de planos e alterado a apiUrl para olhar o heroku

--- qualicorp/src/environments/environment.ts
+++ qualicorp/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://api-qualicorp.herokuapp.com'
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://api-qualicorp.herokuapp.com'
+  // apiUrl: 'http://localhost:3000'
 };
 
 /*
